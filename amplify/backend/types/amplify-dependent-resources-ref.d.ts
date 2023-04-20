afd623ab74ceb828c520d597c902f5f8d2d7a0d3
Andrey Slynko <qkston22@gmail.com>
2023-04-20 14:21:28 +0300

    Add Amplify Authentication for project

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,11 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "Authentication": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
